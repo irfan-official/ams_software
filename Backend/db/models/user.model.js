38bd807ea8d78e3cfc135c1547a7e2b4510741cf
irfan-official <irfanofficial.work@gmail.com>
2025-02-19 22:33:51 +0600

    complete user model

--- Backend/db/models/user.model.js
+++ Backend/db/models/user.model.js
@@ -1,11 +1,26 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-  name: {},
-  email: {},
-  password: {},
-  teacher
-}, { Timestamp: true });
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    teacher,
+  },
+  { Timestamp: true }
+);
 
 const user = mongoose.model("user", userSchema);
 
